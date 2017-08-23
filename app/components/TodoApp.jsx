1bfac078be93f8ae8833f8af1d3bbe56771c1cb6
Sami Kangas <zumartic@gmail.com>
2017-08-23 09:49:30 +0300

    Add statis todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,34 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+    getInitialState: function (){
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: 'Walk the dog'
+                }, {
+                    id: 2,
+                    text: 'Clean the yard'
+                },
+                {
+                    id: 3,
+                    text: 'Wash the car'
+                }, {
+                    id: 4,
+                    text: 'Take out the carbages'
+                }
+            ]
+        };
+    },
     render: function(){
+        var {todos} = this.state;
+
         return (
             <div>
-                <h2>TodoApp.jsx</h2>
+                <h2>TodoApp</h2>
+                <TodoList todos={todos}/>
             </div>
         );
     }
