0e5795587d6c7b1ba97a47b7b7355400d8b54b0d
Sami Kangas <zumartic@gmail.com>
2017-08-25 13:10:15 +0300

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,41 @@ module.exports = {
         }
 
         return $.isArray(todos) ? todos : [];
-    }
+    },
+    filterTodos: function(todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // Filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        // Filter by searchText
+        if (searchText.length>0){
+            filteredTodos = filteredTodos.filter((todo) => {
+                return todo.text.toLowerCase().indexOf(searchText) > -1;
+            });
+        }
+
+        /*
+        filteredTodos = filteredTodos.filter((todo) => {
+            var text = todo.text.toLowerCase();
+
+            return searchText.length === 0 || text.indexOf(searchText) > -1;
+        });
+        */
+
+        // Sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed){
+                return -1;
+            } else if (a.completed && !b.completed){
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return filteredTodos;
+    },
 };
