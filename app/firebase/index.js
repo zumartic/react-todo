7f81d24e3f57ef1e87ae991990e7986eacc69bdf
Sami Kangas <zumartic@gmail.com>
2017-09-08 18:24:38 +0300

    ENV update

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,13 +2,14 @@ import firebase from 'firebase';
 
 try {
     var config = {
-        apiKey: "AIzaSyBDkecrBByIPaCMkC0xxYIZZSatrdcAFgo",
-        authDomain: "mead-todo-app-f7092.firebaseapp.com",
-        databaseURL: "https://mead-todo-app-f7092.firebaseio.com",
-        projectId: "mead-todo-app-f7092",
-        storageBucket: "mead-todo-app-f7092.appspot.com",
-        messagingSenderId: "645525323656"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
+    console.log(config);
     firebase.initializeApp(config);
 } catch (e) {
 
